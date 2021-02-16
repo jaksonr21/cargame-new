fc40c2a8554979a149696e4f3c5172a5b7ba3fd6
jaksonr21 <818381@mystma.org>
2021-02-16 12:42:21 -0600

    finish line
    added finish line

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public float camHeight;
+
+    GameObject Body;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Body = GameObject.Find("Body");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(Body.transform.position.x, camHeight, -10);
+
+
+
+
+
+    }
+}
---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public Rigidbody2D Wheel_F, Wheel_B;
+    public float power;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+            {
+            Wheel_F.AddTorque(-power);
+            Wheel_B.AddTorque(-power);
+             }
+        if (Input.GetKey("s"))
+        {
+            Wheel_F.AddTorque(power);
+            Wheel_B.AddTorque(power);
+        }
+    }
+}
