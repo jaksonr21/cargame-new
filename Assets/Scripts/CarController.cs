2224fc13fee0178169ff10c99768d64279796579
jaksonr21 <818381@mystma.org>
2021-02-22 12:27:25 -0600

    Lots Changed
    Modifications to Track and Respawn Border Added

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-    public Rigidbody2D Wheel_F, Wheel_B;
-    public float power;
+    public Rigidbody2D Wheel_F, Wheel_B, Body;
+    public float power, bodyPower;
 
+    Vector3 startPos;
+
+    Rigidbody2D rb;
+
+    BoxCollider2D bc;
+
+    public float jumpDetectOffset;
+
+    public LayerMask jumpableObjects;
 
     // Start is called before the first frame update
     void Start()
@@ -17,15 +26,49 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        
+        bc = gameObject.GetComponent<BoxCollider2D>();
+        
         if (Input.GetKey("d"))
             {
             Wheel_F.AddTorque(-power);
             Wheel_B.AddTorque(-power);
+			Body.AddTorque(-bodyPower);
              }
         if (Input.GetKey("s"))
         {
             Wheel_F.AddTorque(power);
             Wheel_B.AddTorque(power);
+			Body.AddTorque(bodyPower);
         }
+    }
+     
+    private void OnCollisionEnter2D (Collision2D c)
+    {
+      
+
+        if (c.gameObject.tag == "Spawn")
+        {
+            Body.transform.position = startPos;
+			Body.transform.rotation = Quaternion.identity;
+			Body.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			Body.GetComponent<Rigidbody2D>().angularVelocity = 0;
+			
+			Wheel_F.GetComponent<Rigidbody2D>().angularVelocity = 0;
+			Wheel_F.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			
+			Wheel_B.GetComponent<Rigidbody2D>().angularVelocity = 0;
+			Wheel_B.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(bc.bounds.center, bc.bounds.size, 0f, Vector2.down, jumpDetectOffset, jumpableObjects);
+        Color rayColor = Color.red;
+        Debug.DrawRay(bc.bounds.center - new Vector3(bc.bounds.extents.x, bc.bounds.extents.y + jumpDetectOffset), Vector2.right * bc.bounds.size.x, rayColor);
+        return raycastHit.collider != null;
     }
 }
---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+	
+	GameObject body;
+	Vector3 startPos;
+	GameObject wheel_b;
+	GameObject wheel_f;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GameObject.Find("Body");
+		wheel_f = GameObject.Find("Wheel_F");
+		wheel_b = GameObject.Find("Wheel_B");
+		startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	
+	private void OnCollisionEnter2D(Collision2D c)
+	{
+		if (c.gameObject.tag == "Track")
+		{
+			body.transform.position = startPos;
+			body.transform.rotation = Quaternion.identity;
+			body.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			body.GetComponent<Rigidbody2D>().angularVelocity = 0;
+			
+			wheel_f.GetComponent<Rigidbody2D>().angularVelocity = 0;
+			wheel_f.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			
+			wheel_b.GetComponent<Rigidbody2D>().angularVelocity = 0;
+			wheel_b.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+			
+		}
+	}
+	
+	
+	
+	
+}
